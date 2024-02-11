20b19ecd5e21aee19c4b8180935a482f35a46a66
Farhan Malik <farhanmalik0709@gmail.com>
2024-02-11 11:33:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+var arr = [
+    {name: "Petals of roses", image: "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Animals of town", image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "the crowd of city", image: "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "fruits of planet", image: "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "orange peeled", image: "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "web design", image: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Festivals", image: "https://images.unsplash.com/photo-1581522627590-60362dcf4840?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Coffee beans", image: "https://images.unsplash.com/photo-1625837183025-155d57095fa9?q=80&w=2849&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Clothes", image: "https://images.unsplash.com/photo-1698515129669-7af840870238?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Girlly", image: "https://images.unsplash.com/photo-1696850963955-6c16e9c6a96e?q=80&w=2803&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "books", image: "https://images.unsplash.com/photo-1558901357-ca41e027e43a?q=80&w=3039&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "movies", image: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Babydoll", image: "https://images.unsplash.com/photo-1586907835000-f692bbd4c9e0?q=80&w=3061&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+ 
+]
+
+
+function showTheCards(){
+    var clutter = "";
+    arr.forEach(function(obj){
+        clutter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="image">
+        <div class="caption">Lorem ipsum </div>
+    </div>`;
+    })
+
+    document.querySelector(".container")
+    .innerHTML = clutter;
+}
+
+function handleSearchFunctionality(){
+    var input = document.querySelector("#searchinput");
+    
+
+    input
+    .addEventListener("focus", function(){
+        document.querySelector(".overlay").style.display = "block";
+    })
+
+    input
+    .addEventListener("blur", function(){
+        document.querySelector(".overlay").style.display = "none";
+    })
+
+    input
+    .addEventListener("input", function(){
+        const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        var clutter = "";
+        filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`
+        })
+    document.querySelector(".searchdata").style.display = "block";
+        document.querySelector(".searchdata").innerHTML = clutter;
+    })
+}
+
+handleSearchFunctionality();
+showTheCards();
